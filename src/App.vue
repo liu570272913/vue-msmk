1e04367fd6fed06992308570ab2c817e9374b69d
liubo <570272913@qq.com>
2021-01-21 11:27:00 +0800

    no message

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            };
+        },
+        created() {
+
+        },
+        mounted() {
+
+        },
+        watch: {
+
+        },
+        computed: {
+
+        },
+        methods: {
+
+        }
+    };
+</script>
+
+<style scoped>
+*{
+    font-size: 0.16rem;
+}
+</style>
